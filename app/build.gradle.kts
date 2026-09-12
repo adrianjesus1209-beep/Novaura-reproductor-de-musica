753270d77de4f31bbff44b726da54bd00d14a4b9
Leviat_LVTN <adrianjesus1209@gmail.com>
2026-09-12 11:16:10 -0400

    fix: reemplazo absoluto del logo antiguo en carpetas nodpi e incremento a v1.0.11

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.novaura.music"
         minSdk = 26
         targetSdk = 34
-        versionCode = 11
-        versionName = "1.0.10"
+        versionCode = 12
+        versionName = "1.0.11"
 
         vectorDrawables {
             useSupportLibrary = true
